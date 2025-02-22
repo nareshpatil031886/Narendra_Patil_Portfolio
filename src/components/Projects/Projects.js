946b6ec0a7720e9ce19e02265485e76f03256553
nareshpatil031886 <narendrapatil9247@gmail.com>
2025-02-23 00:38:26 +0530

    done project and  resume section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,89 +8,85 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import aiCourse from "../../Assets/Projects/aiCourse.png";
+import cryptoHunter from "../../Assets/Projects/cryptoHunter.png";
+import textSummarizer from "../../Assets/Projects/textSummarizer.png";
+import movieMeter from "../../Assets/Projects/movieMeter.png";
+import collegeWebsite from "../../Assets/Projects/collegeWebsite.png";
 
 function Projects() {
   return (
-    <Container fluid className="project-section">
+    <Container fluid className='project-section'>
       <Particle />
       <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+        <h1 className='project-heading'>
+          My Recent <strong className='purple'>Works</strong>
         </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+        <p style={{ color: 'white' }}>Here are a few projects I've worked on recently.</p>
+        <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+          {/* AI Course Craft */}
+          <Col md={4} className='project-card'>
             <ProjectCard
-              imgPath={chatify}
+              imgPath={aiCourse}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title='AI Course Craft'
+              description='A Full Stack AI Course Generator built using AdonisJS, Vue.js, and Gemini API. Enables creation of 500+ personalized courses monthly with seamless authentication using Clerk.'
+              ghLink='https://github.com/abhayfaldu/ai-course-craft'
+              demoLink='https://ai-course-craft.vercel.app/'
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* CryptoHunter */}
+          <Col md={4} className='project-card'>
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={cryptoHunter}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title='CryptoHunter'
+              description='A cryptocurrency tracker built with React.js, CoinGecko API, and Material UI. Provides real-time market data, price trends, and a personalized watchlist with Firebase authentication.'
+              ghLink='https://github.com/nareshpatil031886/Crypto-Tracker'
+              demoLink='https://crypto-hunter.netlify.app/'
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* AI-Based Text Summarizer */}
+          <Col md={4} className='project-card'>
             <ProjectCard
-              imgPath={editor}
+              imgPath={textSummarizer}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title='AI-Based Text Summarizer'
+              description='Developed an Article Summarizer using React and integrated OpenAI GPT-3 API, providing concise article summaries while showcasing skills in frontend, state management, and API integration.'
+              ghLink='https://github.com/nareshpatil031886/AI-based-Article-Summarizer'
+              demoLink='https://openaiarticalesummarizer.netlify.app/'
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          {/* Movie Meter Live */}
+          <Col md={4} className='project-card'>
             <ProjectCard
-              imgPath={leaf}
+              imgPath={movieMeter}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title='Movie Meter Live'
+              description='Created a user-friendly movie web app using React and the OMDB API, featuring search functionality, dynamic data retrieval, and responsive design for an intuitive and engaging user experience.'
+              ghLink='https://github.com/nareshpatil031886/Movie_meter'
+              demoLink='https://thapamovie.netlify.app/'
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={4} className='project-card'>
             <ProjectCard
-              imgPath={suicide}
+              imgPath={collegeWebsite}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title='Smart College Website'
+              description='Developed a modern, responsive college website using HTML, CSS, and JavaScript. Showcases college details, events, and an interactive UI to enhance the student experience.'
+              ghLink='https://github.com/nareshpatil031886/Smart-College'
+              demoLink='https://rcpit.netlify.app/'
             />
           </Col>
         </Row>
       </Container>
     </Container>
-  );
+  )
 }
+
 
 export default Projects;
