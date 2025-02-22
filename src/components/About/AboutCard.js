1281b880d5765e58c06d571fbe9d8d8542acb98d
nareshpatil031886 <narendrapatil9247@gmail.com>
2025-02-22 23:55:13 +0530

    About page done

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Narendra Patil </span>
+            from <span className="purple">India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a Full Stack Developer at Webledger.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed My B.Tech In Data Science At R. C. Patel Institute Of Technology, Shirpur.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -34,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Narendra Patil</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,29 +5,30 @@ import {
   SiPostman,
   SiSlack,
   SiVercel,
-  SiMacos,
+  SiWindows,
+  SiGithub,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+    <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
+      <Col xs={4} md={2} className='tech-icons'>
+        <SiWindows />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className='tech-icons'>
         <SiVisualstudiocode />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className='tech-icons'>
         <SiPostman />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className='tech-icons'>
+        <SiGithub />
+      </Col>
+      <Col xs={4} md={2} className='tech-icons'>
         <SiSlack />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
     </Row>
-  );
+  )
 }
 
 export default Toolstack;
