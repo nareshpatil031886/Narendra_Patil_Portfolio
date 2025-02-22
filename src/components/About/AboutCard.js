a78b28d459c3f5a686bb7c60819c9c2d3d4310ab
nareshpatil031886 <narendrapatil9247@gmail.com>
2025-02-23 00:50:53 +0530

    edit about card

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,43 +1,52 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
+import { FaUsers, FaRocket, FaLaptopCode, FaGlobeAsia } from 'react-icons/fa'
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view">
+    <Card className='quote-card-view'>
       <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Narendra Patil </span>
-            from <span className="purple">India.</span>
+        <blockquote className='blockquote mb-0'>
+          <p style={{ textAlign: 'justify' }}>
+            👋 Hi Everyone, I am <span className='purple'>Narendra Patil</span> from
+            <span className='purple'> India 🇮🇳</span>.
             <br />
-            I am currently employed as a Full Stack Developer at Webledger.
+            💻 I am currently working as a <b className='purple'>Full Stack Developer</b> at Webledger.
             <br />
-            I have completed My B.Tech In Data Science At R. C. Patel Institute Of Technology, Shirpur.
+            🎓 I hold a <b className='purple'>B.Tech in Data Science</b> from R. C. Patel Institute Of Technology, Shirpur.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            🚀 Apart from coding, I love to:
           </p>
           <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
+            <li className='about-activity'>
+              🎮 <ImPointRight /> Playing Video Games
             </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+            <li className='about-activity'>
+              ✍️ <ImPointRight /> Writing Tech Blogs
             </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
+            <li className='about-activity'>
+              ✈️ <ImPointRight /> Traveling & Exploring New Places
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Narendra Patil</footer>
+          <br />
+          <p style={{ color: 'rgb(155 126 172)', fontSize: '1.2em' }}>"Strive to build things that make a difference!" 🚀✨</p>
+          <footer className='blockquote-footer'>Narendra Patil</footer>
+          <br />
+
+          {/* Visitor Counter Section */}
+          <div className='visitor-counter'>
+            <FaUsers size={22} color='purple' />
+            <span style={{ marginLeft: '8px', fontSize: '1.2em', color: '#fff' }}>
+              <b className='purple'>1000+</b> Visitors Have Explored This Portfolio! 🌍
+            </span>
+          </div>
         </blockquote>
       </Card.Body>
     </Card>
-  );
+  )
 }
 
 export default AboutCard;
